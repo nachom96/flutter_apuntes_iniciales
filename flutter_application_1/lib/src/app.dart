8ba970d160b4ddc1764eaf79006f27f14d174a0b
Nacho <nacho_085@hotmail.com>
2021-10-21 19:54:08 +0200

    1

---
+++ flutter_application_1/lib/src/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_application_1/src/pages/home_page.dart';
+
+class MyApp extends StatelessWidget{
+
+
+  @override
+  Widget build( context ) {
+
+
+    return MaterialApp(
+      home: Center(
+        child: HomePage(),
+      ) ,
+      );
+
+  }
+
+}
